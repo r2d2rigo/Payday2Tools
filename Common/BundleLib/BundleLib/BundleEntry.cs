25d6fca0a6546a8021ab4caab8649329fdd1f991
r2d2rigo <r2d2rigo@gmail.com>
2014-12-25 03:13:02 +0000

    [BundleLib] Added BundleLib project to source control.

---
+++ Common/BundleLib/BundleLib/BundleEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BundleLib
+{
+    public class BundleEntry
+    {
+        public UInt32 Id { get; private set; }
+        public UInt32 Address { get; private set; }
+        // TODO: fix BundleHeader.Load and make the setter private
+        public Int32 Length { get; set; }
+
+        public BundleEntry(UInt32 id, UInt32 address, Int32 length)
+        {
+            this.Id = id;
+            this.Address = address;
+            this.Length = length;
+        }
+    }
+}
---
+++ Common/BundleLib/BundleLib/BundleHeader.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace BundleLib
+{
+    public class BundleHeader
+    {
+        public List<BundleEntry> Entries { get; private set; }
+
+        public BundleHeader()
+        {
+            this.Entries = new List<BundleEntry>();
+        }
+
+        public static BundleHeader Load(Stream bundleHeaderStream)
+        {
+            BundleHeader loadedHeader = new BundleHeader();
+
+            try
+            {
+                using (BinaryReader headerReader = new BinaryReader(bundleHeaderStream))
+                {
+                    headerReader.ReadUInt32();
+                    headerReader.ReadUInt32();
+
+                    UInt32 bundleEntryCount = headerReader.ReadUInt32();
+
+                    headerReader.ReadUInt32();
+                    
+                    bool hasEntryLength = headerReader.ReadUInt32() == 24;
+
+                    if (hasEntryLength)
+                    {
+                        bundleHeaderStream.Position += 2 * 4;
+                    }
+
+                    for (int i = 0; i < bundleEntryCount; i++)
+                    {
+                        UInt32 id = headerReader.ReadUInt32();
+                        UInt32 address = headerReader.ReadUInt32();
+                        Int32 length = 0;
+
+                        if (hasEntryLength)
+                        {
+                            length = headerReader.ReadInt32();
+                        }
+                        else
+                        {
+                            if (i == (bundleEntryCount - 1))
+                            {
+                                // TODO: check that this is a valid value.
+                                length = -1;
+                            }
+                            else if (i > 0)
+                            {
+                                BundleEntry previousEntry = loadedHeader.Entries[i - 1];
+                                previousEntry.Length = (Int32)(address - previousEntry.Address);
+                            }
+                        }
+
+                        BundleEntry newEntry = new BundleEntry(id, address, length);
+
+                        loadedHeader.Entries.Add(newEntry);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception loading bundle header: " + ex.Message);
+
+                return null;
+            }
+
+            return loadedHeader;
+        }
+    }
+}
